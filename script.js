5d1a105e53895366ace3b17f03480bf305326064
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-04-04 13:42:30 -0500

    Send info to console

--- script.js
+++ script.js
@@ -24,4 +24,11 @@ function sortList() {
 	}
 }
 
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
 // document.getElementById("websiteURL").innerHTML = window.location.href;
