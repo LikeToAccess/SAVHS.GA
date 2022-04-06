d348f3b7f8d28a38fd14efb017a741f32b7fa84e
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-04-06 07:00:24 -0500

    Broke the login

--- lunch/script.js
+++ lunch/script.js
@@ -38,3 +38,18 @@ var current_lunch_link = document.getElementById("current_lunch_json");
 var current_lunch_script = document.getElementById("current_lunch_script");
 current_lunch_link.setAttribute("href", current_lunch_link_url);
 current_lunch_script.setAttribute("src", current_lunch_link_url + ".js");
+
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
+}
