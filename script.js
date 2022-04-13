dc5e83b2141db6a9ddcd7c1b9f98d63a19ef6780
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-04-13 12:37:35 -0500

    Added test thing

--- script.js
+++ script.js
@@ -74,6 +74,7 @@ function onSignIn(googleUser) {
 		].includes(window.location.href)
 	) {
 		// displayAccountDetails(id, name, imageUrl, email);
+		console.log("TEST");
 		document.getElementById("sketch-script").setAttribute("src", "sketch.js");
 	}
 }
