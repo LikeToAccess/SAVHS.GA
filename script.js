91454f05d42acc931bfc07331e5294b512eed112
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-04-14 10:06:02 -0500

    Broke flap page

--- script.js
+++ script.js
@@ -78,7 +78,8 @@ function onSignIn(googleUser) {
 		document.getElementById("flap-h1").innerText = "Flap Bird";
 		document.getElementById("flap-p").removeAttribute("hidden");
 		// document.getElementById("sketch-script").setAttribute("src", "sketch.js");
-		var scriptTag = document.createElement("script").setAttribute("src", "sketch.js");
+		var scriptTag = document.createElement("script")
+		scriptTag.src = "sketch.js";
 		var head = document.getElementsByTagName("head")[0];
 		head.appendChild(scriptTag);
 	}
