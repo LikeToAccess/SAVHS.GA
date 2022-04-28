336a2e9038c2bbcc28572910666bc1cfe303a26d
LikeToAccess <375916737591675+LikeToAccess@users.noreply.github.com>
2022-04-28 10:44:18 -0500

    Added RIP code

---
+++ scripts/RIP/settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# filename          : settings.py
+# description       : Different options for main.py
+# author            : Ian Ault
+# email             : liketoaccess@protonmail.com
+# date              : 04-27-2022
+# version           : v1.0
+# usage             : python main.py profile [profile ...]
+# notes             : This file should not be run directly
+# license           : MIT
+# py version        : 3.10.2
+#==============================================================================
+# Login information for an Instagram account.
+# This is used to prevent Instagram from asking for a login while viewing posts
+# and to get information off of Private Profiles.
+# The default values are None.
+USERNAME = None
+PASSWORD = None
