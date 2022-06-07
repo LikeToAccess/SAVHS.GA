59f4e5b531823aa1258802b8f9b1b9ad1802efe4
LikeToAccess <37591675+LikeToAccess@users.noreply.github.com>
2022-06-07 17:57:38 -0500

    Fixed all code!

--- script.js
+++ script.js
@@ -185,9 +185,7 @@ function onSignIn(googleUser) {
 	const formElement = document.getElementById("form-id");
 	formElement.addEventListener("submit", async function (e) {
 		e.preventDefault();
-		const formData = new FormData(formElement).entries()
-		console.log(formData);
-		quote = formData["quote"]
+		quote = document.getElementById("quote-id").value;
 		const response = await fetch("http://api.savhs.ga:8080/quotes?email="+ email +"&name="+ name +"&quote="+ quote +"&profile_picture="+ imageUrl, {
 			method: "POST"
 			// headers: { "Content-Type": "application/json" },
